82cdc6a6372eb9efa0116710a737f07b1eba6f41
xmuyqy <544797138@qq.com>
2024-12-02 22:10:22 +0800

    aaa

--- apps/webget.cc
+++ apps/webget.cc
@@ -9,8 +9,18 @@ using namespace std;
 
 void get_URL( const string& host, const string& path )
 {
-  cerr << "Function called: get_URL(" << host << ", " << path << ")\n";
-  cerr << "Warning: get_URL() has not been implemented yet.\n";
+  TCPSocket socket;
+  socket.connect(Address(host, "http"));
+  socket.write("GET " + path + " HTTP/1.1\r\n");
+  socket.write("Host: " + host + "\r\n");
+  socket.write("Connection: close\r\n\r\n");
+  while (!socket.eof()) {
+    string str;
+    socket.read(str);
+    cout << str;
+  }
+  socket.close();
+
 }
 
 int main( int argc, char* argv[] )
